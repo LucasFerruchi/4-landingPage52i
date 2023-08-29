9d4712f21f5e804eb8e46c4349f4494160dd71bf
LucasFerruchi <lucasferruchi05@gmail.com>
2023-08-29 16:44:28 -0300

    funcion clima

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//importar funcion
+import { obtenerClima } from "../helpers/obtener-clima";
 
 const NavBar = () => {
+  //Estado para manejar el clima
+  const [tiempo, setTiempo] = useState(null);
+
+  //useEffect -- ejecuta en el montaje del componente
+  useEffect(() => {
+    clima();
+  }, []);
+
+  const clima = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const coords = pos.coords;
+      const lat = coords.latitude;
+      const long = coords.longitude;
+
+      obtenerClima(lat, long)
+        .then((respuesta) => {
+          const { main, weather } = respuesta;
+          setTiempo({
+            temp: main.temp,
+            clima: weather[0],
+          });
+        })
+        .catch((error) => console.log(error));
+    });
+  };
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Inicio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contactos
-                </a>
-              </li>
-            </ul>
+      <div className="fixed-top">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Inicio
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contactos
+                  </a>
+                </li>
+              </ul>
+              {/* DATOS DEL CLIMA */}
+              {tiempo && (
+                <div className="d-flex justify-content-center align-items-center ">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${tiempo.clima.icon}@2x.png`}
+                    alt="clima"
+                  />
+                  <span>{tiempo.temp}°C</span>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,66 +1,90 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// //Nuevas dependencias
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.js";
-// import "font-awesome/css/font-awesome.css";
-// // import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+//Nuevas dependencias
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "font-awesome/css/font-awesome.css";
+// import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-//----------------------------------------------------------
-//PROMESAS
+// //----------------------------------------------------------
+// //PROMESAS
 
-const generarInforme = () => {
-  return new Promise((resolve, reject) => {
-    let informe = false; //ej de respuesta
+// const generarInforme = () => {
+//   return new Promise((resolve, reject) => {
+//     let informe = false; //ej de respuesta
 
-    setTimeout(() => {
-      if (informe) {
-        resolve("Recibimos el informe 🤩");
-      } else {
-        reject("Error 😪");
-      }
-    }, 2000);
-  });
-};
+//     setTimeout(() => {
+//       if (informe) {
+//         resolve("Recibimos el informe 🤩");
+//       } else {
+//         reject("Error 😪");
+//       }
+//     }, 2000);
+//   });
+// };
 
-//resultado
-/*
-then: recibir resultdo positivo
-catch: recibe error
-finally: recibe ambos, pero hay q configurar
-*/
+// //resultado
+// /*
+// then: recibir resultdo positivo
+// catch: recibe error
+// finally: recibe ambos, pero hay q configurar
+// */
 
-generarInforme()
-  .then((respuesta) => console.log(respuesta))
-  .catch((error) => console.log(error));
+// generarInforme()
+//   .then((respuesta) => console.log(respuesta))
+//   .catch((error) => console.log(error));
 
-console.log(generarInforme());
+// console.log(generarInforme());
 
-//por FETCH-----------------------------------------------
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=a7fc87f5b94b227523a49f35835492c1"
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// //por FETCH-----------------------------------------------
+// fetch(
+//   "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=a7fc87f5b94b227523a49f35835492c1"
+// )
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
 
-//PROMESAS--------------------------------------------------------------------------
+// //PROMESAS--------------------------------------------------------------------------
 
-//!ASYNC/AWAIT------------------------------------------------------------------------------------------------------------
-const obtenerClima = async () => {
-  const resp = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=a7fc87f5b94b227523a49f35835492c1"
-  );
-  const data = await resp.json();
+// //!ASYNC/AWAIT------------------------------------------------------------------------------------------------------------
+// const obtenerClima = async () => {
+//   const resp = await fetch(
+//     "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=a7fc87f5b94b227523a49f35835492c1"
+//   );
+//   const data = await resp.json();
 
-  return data;
-};
+//   return data;
+// };
 
-obtenerClima().then((respuesta) => console.log(respuesta));
-//!ASYNC/AWAIT------------------------------------------------------------------------------------------------------------
+// obtenerClima().then((respuesta) => console.log(respuesta));
+// //!ASYNC/AWAIT------------------------------------------------------------------------------------------------------------
+
+// //Obtener localizacion
+// const clima = () => {
+//   navigator.geolocation.getCurrentPosition((pos) => {
+//     const coords = pos.coords;
+//     const lat = coords.latitude;
+//     const long = coords.longitude;
+
+//     obtenerClima(lat, long)
+//       .then((respuesta) => console.log(respuesta))
+//       .catch((error) => console.log(error));
+//   });
+// };
+
+// const obtenerClima = async (lat, long) => {
+//   const resp = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=a7fc87f5b94b227523a49f35835492c1`
+//   );
+//   const data = await resp.json();
+
+//   return data;
+// };
+
+// clima();
