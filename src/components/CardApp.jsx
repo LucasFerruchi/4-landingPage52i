4a0715b5373f665d573e566421c0c543ab471d3b
Augusto Grosso <grossoaugusto4@gmail.com>
2023-09-04 18:35:29 -0300

    Estilo de card para el modo oscuro

--- src/components/CardApp.jsx
+++ src/components/CardApp.jsx
@@ -4,14 +4,14 @@ const CardApp = ({ item, modoOscuro }) => {
   return (
     <>
       <div className="col-12 col-md my-3">
-        <div className={`card h-100 ${modoOscuro}`}>
+        <div className={`card h-100 ${modoOscuro? "text-bg-dark": ""}`}>
           <div className="card-body d-flex flex-column align-items-center">
             <i
               className={`fa ${item.icono} fa-3x text-primary`}
               aria-hidden="true"
             ></i>
-            <p className="text-muted fs-3 fs-md-2">{item.texto1}</p>
-            <p className="text-muted">{item.texto2}</p>
+            <p className={` fs-3 fs-md-2 ${modoOscuro? "text-light":"text-muted"}`}>{item.texto1}</p>
+            <p className={` ${modoOscuro? "text-light":"text-muted"}`}>{item.texto2}</p>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -58,7 +58,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 //     "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=a7fc87f5b94b227523a49f35835492c1"
 //   );
 //   const data = await resp.json();
-
 //   return data;
 // };
 
