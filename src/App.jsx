2e3ed9600495b9dbbececc3fe8137f34168f4e18
Sergio Ocampo <sergioocampo1@gmail.com>
2023-09-04 21:08:50 -0300

    se creo rama pages y sistema de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,23 @@ import { detalleCard } from "./data/info.js";
 import NavBar from "./components/NavBar";
 import CarouselApp from "./components/CarouselApp";
 import CardApp from "./components/CardApp";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import ContactoScreen from "./pages/ContactoScreen.jsx";
+import InicioScreem from "./pages/InicioScreen.jsx";
+import ErrorScreen from "./pages/ErrorScreen.jsx";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="contacto" element={<ContactoScreen/>} />
+        <Route path="/" element={<InicioScreem/>} />
+        <Route path="*" element={<ErrorScreen/>} />
+
+      </Routes>
+      
+      </BrowserRouter>
       <NavBar />
       <CarouselApp />
       <div className="container">
---
+++ src/pages/InicioScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InicioScreem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InicioScreem
